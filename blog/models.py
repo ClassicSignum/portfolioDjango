562a38672ddfe38e2699c9d34cf97a15aa5884e2
nafiz <nafizurrahman001@gmail.com>
2020-07-25 15:32:28 +0600

    added homepage to job templates and migrated

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+# after creating a new model make migrations (python manage.py makemigrations)
+#then migrate (python manage.py migrate)
+#and if you want to add this model to admin then go to ..
+# this app's admin.py and put this code [
+
+
+#from .models import Blog
+ #admin.site.register(Blog)
+
+#]
